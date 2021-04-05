65923e7931c9ffea742d987dfd4c5fa840aa8537
Vinnik-Danil <58749648+Vinnik-Danil@users.noreply.github.com>
2021-04-05 17:48:33 +0300

    beta1

--- .sfdx/typings/lwc/apex/AnomalyViewComponentController.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@salesforce/apex/AnomalyViewComponentController.getAnomalies" {
-  export default function getAnomalies(): Promise<any>;
-}
-declare module "@salesforce/apex/AnomalyViewComponentController.getCurrentWeather" {
-  export default function getCurrentWeather(): Promise<any>;
-}
-declare module "@salesforce/apex/AnomalyViewComponentController.generateAnomalyReport" {
-  export default function generateAnomalyReport(): Promise<any>;
-}
